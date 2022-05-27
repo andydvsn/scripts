92fa9afa50e13860da6a569c47362355e11fe9e6
Andrew Davison <andydvsn@users.noreply.github.com>
2022-05-27 01:26:35 +0100

    github_pull_all.sh

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Scripts
+
+Personal scripts which may be of little to no use for anybody else.
---
+++ debian/github_pull_all.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# github_pull_all.sh
+#  Loop through the directories of a given path and update all repos.
+
+if [ $# -lt 1 ]; then
+	echo "Usage: $0 <path>"
+	echo
+	exit 1
+fi
+
+dest="$1"
+
+for d in $(find "$dest" -mindepth 1 -maxdepth 1 -type d); do
+	echo $(date +'%Y-%m-%d-%H:%M:%S') : "$d" > "$dest/log.txt"
+	git -C "$d" stash &>>"$dest/log.txt"
+	git -C "$d" pull --rebase &>>"$dest/log.txt"
+	echo >> "$dest/log.txt"
+done
