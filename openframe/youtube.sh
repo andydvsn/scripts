78d12820512fc985be381d3d2d734c839fae7720
Andrew Davison <andydvsn@users.noreply.github.com>
2025-02-08 23:05:11 +0000

    Correct a test thing

--- openframe/youtube.sh
+++ openframe/youtube.sh
@@ -2,6 +2,6 @@
 
 ## youtube.sh v0.01 (31st January 2025) by Andrew Davison
 
-yt-dlp --hls-use-mpegts --merge-output-format mp4 -f "bv*[height<=480]+ba" "$1" -o - | mplayer -
+yt-dlp -f "bv*[height<=480]+ba" "$1" -o - | mplayer -
 
 exit 0
