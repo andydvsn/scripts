1faf8ce05b49244ade5e4901849552ca976ffbb2
Andrew Davison <andydvsn@users.noreply.github.com>
2025-02-08 22:19:23 +0000

    Tweak

--- openframe/unsplash.sh
+++ openframe/unsplash.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 ## unsplash.sh v0.01 (31st January 2025) by Andrew Davison
-##  Mucking about with FBI.
 
 wget -q -O /tmp/unsplash.jpg https://unsplash.it/800/480/?random
 display -window root /tmp/unsplash.jpg
