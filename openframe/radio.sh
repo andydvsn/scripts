374a2f2045fb6654e4b6f43f21b3d6440bb0efb3
Andrew Davison <andydvsn@users.noreply.github.com>
2025-02-12 13:07:22 +0000

    Channel twiddling

--- openframe/radio.sh
+++ openframe/radio.sh
@@ -7,6 +7,7 @@
 hdhrip="192.168.11.103"
 channel="707"
 
+# Keep an eye on our restart count.
 if [ -f /tmp/radio_restarts ]; then
 	restarts=$(cat /tmp/radio_restarts)
 	let restarts++
@@ -15,6 +16,7 @@ else
 	echo 0 > /tmp/radio_restarts
 fi
 
+# Set the channel from the file if present.
 if [ -f /tmp/radio_channel ]; then
 	channel=$(cat /tmp/radio_channel)
 else
@@ -22,13 +24,14 @@ else
 	chmod 666 /tmp/radio_channel
 fi
 
-mplayer -novideo -cache 1024 -cache-min 80 http://$hdhrip:5004/auto/v$channel &
-
+# Watch the channel file for changes.
 while inotifywait -e close_write /tmp/radio_channel; do
 
 	systemctl restart radio.service
 
 done &
+
+mplayer -novideo -cache 1024 -cache-min 80 http://$hdhrip:5004/auto/v$channel &
 
 while true; do
 
