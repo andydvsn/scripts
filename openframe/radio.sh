bb749379a207d70c6a7f5824124fd326b4da41c4
Andrew Davison <andy@users.noreply.github.com>
2025-02-13 15:06:51 +0000

    Better play pause control.

--- openframe/radio.sh
+++ openframe/radio.sh
@@ -1,66 +1,74 @@
 #!/usr/bin/env bash
 
-## radio.sh v1.01 (12th February 2025)
+## radio.sh v1.02 (13th February 2025)
 ##  Streams the radio from a HDHomeRun and restarts it if there's a problem.
 ##  Use the accompanying radio.service file.
 
 hdhrip="192.168.11.103"
 channel="707"
+tmploc="/tmp/radio"
+
+# Set up our temporary location with access for any audio group member.
+if [ ! -d $tmploc ]; then
+	mkdir -p $tmploc
+	chown :audio $tmploc
+	chmod 777 $tmploc
+	chmod g+s $tmploc
+	umask 002 $tmploc
+fi
 
 # Handle play, pause and channel selection.
 if [ "$1" == "pause" ]; then
-	[ -f /tmp/radio_channel ] && cp /tmp/radio_channel /tmp/radio_paused
-	echo 0 > /tmp/radio_channel
-	chmod 666 /tmp/radio_channel
+	[ -f $tmploc/channel ] && cp $tmploc/channel $tmploc/paused
+	echo 0 > $tmploc/channel
 	exit 0
 elif [ "$1" == "play" ]; then
-	if [ -f /tmp/radio_paused ]; then
-		cp /tmp/radio_paused /tmp/radio_channel
-		chmod 666 /tmp/radio_channel
+	if [ -f $tmploc/paused ]; then
+		cp $tmploc/paused $tmploc/channel
+		rm $tmploc/paused
 	else
-		echo $channel > /tmp/radio_channel
-		chmod 666 /tmp/radio_channel
+		echo $channel > $tmploc/channel
 	fi
 	exit 0
 elif [[ "$1" =~ ^[+-]?[0-9]+$ ]]; then
-	echo $1 > /tmp/radio_channel
-	chmod 666 /tmp/radio_channel
+	echo $1 > $tmploc/channel
 	exit 0
 elif [[ "$1" != "" ]]; then
 	exit 1
 fi
 
 # Keep an eye on our restart count.
-if [ -f /tmp/radio_restarts ]; then
-	restarts=$(cat /tmp/radio_restarts)
+if [ -f $tmploc/restarts ]; then
+	restarts=$(cat $tmploc/restarts)
 	let restarts++
-	echo $restarts > /tmp/radio_restarts
-	chmod 666 /tmp/radio_restarts
+	echo $restarts > $tmploc/restarts
 else
-	echo 0 > /tmp/radio_restarts
-	chmod 666 /tmp/radio_restarts
+	echo 0 > $tmploc/restarts
 fi
 
 # Set the channel from the file if present.
-if [ -f /tmp/radio_channel ]; then
-	channel=$(cat /tmp/radio_channel)
+if [ -f $tmploc/channel ]; then
+	channel=$(cat $tmploc/channel)
 else
-	echo $channel > /tmp/radio_channel
-	chmod 666 /tmp/radio_channel
+	echo $channel > $tmploc/channel
 fi
 
 # Watch the channel file for changes.
-while inotifywait -e close_write /tmp/radio_channel; do
+while inotifywait -e close_write $tmploc/channel; do
 
 	systemctl restart radio.service
 
 done &
 
-mplayer -novideo -cache 1024 -cache-min 80 http://$hdhrip:5004/auto/v$channel &
+mplayer -novideo -cache 512 -cache-min 80 http://$hdhrip:5004/auto/v$channel &
 
 while true; do
 
 	sleep 10
-	journalctl -n 6 -u radio.service | grep -i "Cache empty" && systemctl restart radio.service
+	log=$(journalctl -n 6 -u radio.service)
+	
+	if [[ "$log" =~ "Cache empty" ]] || [[ "$log" =~ "Network is unreachable" ]]; then
+		systemctl restart radio.service
+	fi
 
 done
