fd65d2aa6a9879ec0f38fe93a825ec880b61c4e4
Andrew Davison <andydvsn@users.noreply.github.com>
2025-02-16 22:25:56 +0000

    Update trmnl.sh

--- openframe/trmnl.sh
+++ openframe/trmnl.sh
@@ -1,14 +1,30 @@
 #!/usr/bin/env bash
 
-## trmnl.sh v0.01 (8th February 2025) by Andrew Davison
+## trmnl.sh v0.02 (8th February 2025) by Andrew Davison
 
-source ~/.trmnl_key
+source ~/.trmnl_key2
 
-response=$(curl -s https://usetrmnl.com/api/display --header "access-token:$trmnl_key")
+response=$(curl -s https://usetrmnl.com/api/display --header "access-token:$trmnl_key" --header "fw-version:9.9.9")
 image_url=$(echo "$response" | jq -r '.image_url')
 
+[ "$1" == "debug" ] && echo "$response" | jq
+
 wget -q -O /tmp/trmnl.bmp "$image_url"
-convert /tmp/trmnl.bmp -negate /tmp/trmnl.bmp
-display -window root /tmp/trmnl.bmp
+
+if which display > /dev/null 2>&1; then
+
+	#convert /tmp/trmnl.bmp -negate /tmp/trmnl.bmp
+	display -window root /tmp/trmnl.bmp
+
+elif which fbi > /dev/null 2>&1; then
+
+	fbi -vt 1 -noverbose /tmp/trmnl.bmp > /dev/null 2>&1
+
+else
+
+	echo "I have no way to display the image!"
+	exit 1
+
+fi
 
 exit 0
