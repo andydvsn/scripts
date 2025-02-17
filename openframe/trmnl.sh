5228599f9f319f9909407ddca92ba376d88cd091
Andrew Davison <andy@users.noreply.github.com>
2025-02-17 13:18:04 +0000

    Error handling because I'm having an error.

--- openframe/trmnl.sh
+++ openframe/trmnl.sh
@@ -1,14 +1,44 @@
 #!/usr/bin/env bash
 
-## trmnl.sh v0.01 (8th February 2025) by Andrew Davison
+## trmnl.sh v0.03 (17th February 2025) by Andrew Davison
 
 source ~/.trmnl_key
 
-response=$(curl -s https://usetrmnl.com/api/display --header "access-token:$trmnl_key")
+response=$(curl -s https://usetrmnl.com/api/display \
+	--header "access-token:$trmnl_key" \
+	--header "battery-voltage:3.83" \
+	--header "fw-version:6.9" \
+	--header "rssi:-69")
 image_url=$(echo "$response" | jq -r '.image_url')
 
-wget -q -O /tmp/trmnl.bmp "$image_url"
-convert /tmp/trmnl.bmp -negate /tmp/trmnl.bmp
-display -window root /tmp/trmnl.bmp
+[ "$1" == "debug" ] && echo "$response" | jq
+
+if [ "$image_url" != "null" ]; then
+
+	wget -q -O /tmp/trmnl.bmp "$image_url"
+
+	if which display > /dev/null 2>&1; then
+
+		#convert /tmp/trmnl.bmp -negate /tmp/trmnl.bmp
+		display -window root /tmp/trmnl.bmp
+
+	elif which fbi > /dev/null 2>&1; then
+
+		fbi -vt 1 -noverbose /tmp/trmnl.bmp > /dev/null 2>&1
+
+	else
+
+		echo "No means to display the image. Please install fbi or imagemagick."
+		exit 1
+
+	fi
+
+else
+
+		[ "$1" != "debug" ] && echo "$response" | jq
+		echo "Failed to fetch image."
+		exit 1
+
+fi
 
 exit 0
