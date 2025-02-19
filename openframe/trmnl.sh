80a14d3b0bc3350fdc07e055dbb190c100342de7
Andrew Davison <andydvsn@users.noreply.github.com>
2025-02-19 18:01:15 +0000

    Working a bit better

--- openframe/trmnl.sh
+++ openframe/trmnl.sh
@@ -3,46 +3,50 @@
 ## trmnl.sh v0.04 (19th February 2025) by Andrew Davison
 
 source ~/.trmnl_key
+image="/home/$USER/scripts/openframe/trmnl/happy.png"
 
 while true; do
 
-	response=$(curl -s https://usetrmnl.com/api/display \
+	# Try to get the image through the API.
+	response=$(curl -s -m 10 https://usetrmnl.com/api/display \
 		--header "access-token:$trmnl_key" \
 		--header "battery-voltage:3.83" \
 		--header "fw-version:6.9" \
 		--header "rssi:-69")
 	image_url=$(echo "$response" | jq -r '.image_url')
 	refresh_rate=$(echo "$response" | jq -r '.refresh_rate')
+	[ "$refresh_rate" != "" ] || refresh_rate=6
 
 	[ "$1" == "debug" ] && echo "$response" | jq
+	echo "image_url = $image_url"
+	echo "refresh_rate = $refresh_rate"
 
-	if [ "$image_url" != "null" ]; then
+	# Got the URL? Grab the file, update the path.
+	if [ "$image_url" != "" ]; then
 
-		wget -q -O /tmp/trmnl.bmp "$image_url" || rm /tmp/trmnl.bmp
-
-		if which display > /dev/null 2>&1; then
-
-			#convert /tmp/trmnl.bmp -negate /tmp/trmnl.bmp
-			display -window root /tmp/trmnl.bmp
-
-		elif which fbi > /dev/null 2>&1; then
-
-			pid=$(pgrep fbi)
-			[ -f /tmp/trmnl.bmp ] && fbi -vt 1 -noverbose /tmp/trmnl.bmp > /dev/null 2>&1
-			[ "$pid" != "" ] && sleep 1 && kill $pid
-
+		wget -q -O /tmp/trmnl.bmp "$image_url"
+		if [ $? -ne 0 ]; then
+			refresh_rate=10
 		else
-
-			echo "No means to display the image. Please install fbi or imagemagick."
-			exit 1
-
+			image="/tmp/trmnl.bmp"
 		fi
+
+	fi
+
+	# Try to display the image.
+	if which display > /dev/null 2>&1; then
+
+		#convert $image -negate $image
+		display -window root $image
+
+	elif which fbi > /dev/null 2>&1; then
+
+		fbi -vt 1 -noverbose -t $refresh_rate -1 $image > /dev/null 2>&1 &
 
 	else
 
-			[ "$1" != "debug" ] && echo "$response" | jq
-			echo "Failed to fetch image."
-			exit 1
+		echo "No means to display the image. Please install fbi or imagemagick."
+		exit 1
 
 	fi
 
